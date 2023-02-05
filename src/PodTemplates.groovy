ad250a897d8a511ad8d49ff10d17c781420ca91d
Manoj Chaulagain <chaulagainmanoj45@gmail.com>
2023-02-05 00:41:22 -0800

    second commit

--- src/PodTemplates.groovy
+++ src/PodTemplates.groovy
@@ -1,18 +1,15 @@
 public void dockerTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'sleep', args: '99d')],
-        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+        containers: [containerTemplate(name: 'docker', image: 'docker', command: '', args: '')]) {
     body.call()
-}
+        }
 }
 
 public void mavenTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'sleep', args: '99d')],
-        volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
-                  persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2repo')]) {
+        containers: [containerTemplate(name: 'maven', image: 'maven', command: '', args: '')]) {
     body.call()
-}
+        }
 }
 
 return this
