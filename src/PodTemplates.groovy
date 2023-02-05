b1163c056906e0e4027634fc09745201a42b4bb0
Manoj Chaulagain <chaulagainmanoj45@gmail.com>
2023-02-04 16:33:34 -0800

    Update PodTemplates.groovy

--- src/PodTemplates.groovy
+++ src/PodTemplates.groovy
@@ -1,6 +1,6 @@
 public void dockerTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'sleep', args: '99d')],
+        containers: [containerTemplate(name: 'docker', image: 'docker', command: '', args: '')],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     body.call()
 }
@@ -8,7 +8,7 @@ public void dockerTemplate(body) {
 
 public void mavenTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'sleep', args: '99d')],
+        containers: [containerTemplate(name: 'maven', image: 'maven', command: '', args: '')],
         volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
                   persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2repo')]) {
     body.call()
