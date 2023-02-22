a10f5f5235808251e09dc3b1270fdad7bbb42b8c
Manoj Chaulagain <chaulagainmanoj45@gmail.com>
2023-02-22 01:06:35 -0800

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,11 +4,11 @@ println("Hello World")
 println("ls".execute().text)
 println("printenv".execute().text)
 println("ls -al /var/jenkins_home/workspace/test-pipeline".execute().text)
-println("ls -al /var/jenkins_home/workspace/test-jobs/test-pipeline".execute().text)
+println("ls -al /var/jenkins_home/workspace/testjobs/test-pipeline".execute().text)
 
 
 
-new File(System.getenv('JENKINS_HOME') + '/workspace/test-jobs/test-pipeline').eachFileRecurse(FILES) {
+new File(System.getenv('JENKINS_HOME') + '/workspace/testjobs/test-pipeline').eachFileRecurse(FILES) {
     if(it.name.endsWith('.groovy')) {
         println it
     }
