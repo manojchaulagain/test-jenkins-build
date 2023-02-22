f73fd5e7304d406d562ec3a75d0556e2da7c01e8
Manoj Chaulagain <chaulagainmanoj45@gmail.com>
2023-02-21 23:52:58 -0800

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,5 +1,7 @@
 println("Hello World")
 println("ls".execute().text)
 println("printenv".execute().text)
+println("ls -al /var/jenkins_home/workspace/test-pipeline".execute().text)
+
 def workspace = pwd()
 println(workspace)
