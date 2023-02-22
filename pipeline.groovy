0af1d88274a9084842f2b32f9ea81473babfec17
Manoj Chaulagain <chaulagainmanoj45@gmail.com>
2023-02-21 23:56:01 -0800

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,7 +1,14 @@
+import static groovy.io.FileType.FILES
+
 println("Hello World")
 println("ls".execute().text)
 println("printenv".execute().text)
 println("ls -al /var/jenkins_home/workspace/test-pipeline".execute().text)
 
-def workspace = pwd()
-println(workspace)
+
+
+new File('/var/jenkins_home/workspace/test-pipeline').eachFileRecurse(FILES) {
+    if(it.name.endsWith('.groovy')) {
+        println it
+    }
+}
